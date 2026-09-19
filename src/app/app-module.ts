3c0e3ee189a2b4cbc63a45e11b23855fd3b476fa
IEVN1002-23001155 <85879@alumnos.utleon.edu.mx>
2026-09-18 20:51:58 -0600

    ng mode 18/09/2026

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -3,10 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing-module';
 import { App } from './app';
 import { HeroesList } from './heroes/heroes-list/heroes-list';
+//import { NgOptimizedImage } from '../../node_modules/@angular/common/types/common';
+import { NgOptimizedImage } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [App, HeroesList],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, NgOptimizedImage,FormsModule],
   providers: [provideBrowserGlobalErrorListeners()],
   bootstrap: [App],
 })
--- src/app/heroes/heroes-list/heroes-list.ts
+++ src/app/heroes/heroes-list/heroes-list.ts
@@ -6,4 +6,45 @@ import { Component } from '@angular/core';
   styleUrl: './heroes-list.css',
   templateUrl: './heroes-list.html',
 })
-export class HeroesList {}
+export class HeroesList {
+
+  imageWidth:number=40;
+  imageMargin:number=2;
+  muestraImagen:boolean=true;
+  listFilter:string='';
+
+  showImage():void{
+    this.muestraImagen=!this.muestraImagen
+  }
+
+  heroes:any[]=[
+    {
+
+    imagen:'https://dragonball-api.com/characters/goku_normal.webp',
+    nombre:"Goku",
+    descripcion:"Saiyan - Male ",
+    race:"Saiyan - Male",
+    ki:60000000
+    },{
+      imagen:'https://dragonball-api.com/characters/Freezer.webp',
+    nombre:"Freezer",
+    descripcion:"Army of Frieza ",
+    race:"Frieza Race - Male",
+    ki:530000
+    },{
+      imagen:'https://dragonball-api.com/transformaciones/Broly_DBS_Base.webp',
+    nombre:"Broly",
+    descripcion:"Saiyan - Male  ",
+    race:"Saiyan - Male",
+    ki:7000000000000000
+    },{
+      imagen:'https://dragonball-api.com/transformaciones/gogeta.webp',
+    nombre:"Gogeta",
+    descripcion:"Z Fighter",
+    race:"Saiyan - Male",
+    ki:280000000000000
+    }
+    
+    
+  ]
+}
